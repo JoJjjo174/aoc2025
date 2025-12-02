e26096a9a79096412476d6f39b925f722ff22bdd
Jonathan S <95241309+JoJjjo174@users.noreply.github.com>
2025-12-02 08:31:05 +0100

    day 2 part 2

--- 2/main.py
+++ 2/main.py
@@ -1,4 +1,4 @@
-
+import textwrap
 
 def get_invalid_ids(ids):
 
@@ -6,8 +6,6 @@ def get_invalid_ids(ids):
 
     for id in ids:
         str_id = str(id)
-
-
         id_length = len(str_id)
         if id_length % 2 != 0:
             continue
@@ -17,6 +15,27 @@ def get_invalid_ids(ids):
 
         if p1 == p2:
             invalid_sum += id
+
+    return invalid_sum
+
+def get_invalid_ids_p2(ids):
+
+    invalid_sum = 0
+
+    for id in ids:
+        str_id = str(id)
+        id_length = len(str_id)
+
+        for it in range(1, id_length+1):
+
+            if id_length % it == 0 and it != 1:
+                divisor = it
+
+                parts = textwrap.wrap(str_id, int(id_length/divisor))
+
+                if len(set(parts)) == 1:
+                    invalid_sum += id
+                    break
 
     return invalid_sum
 
@@ -47,6 +66,10 @@ all_ids = extract_ids(ranges)
 
 print(
     get_invalid_ids(all_ids)
+)
+
+print(
+    get_invalid_ids_p2(all_ids)
 )
 
 
