58f0de5e2d81cb5a60f6f8e45355812af61d78da
Jonathan S <95241309+JoJjjo174@users.noreply.github.com>
2025-12-07 12:00:20 +0100

    d7p2

--- 7/main.py
+++ 7/main.py
@@ -40,6 +40,75 @@ def p1(grid):
 
     return splits
 
+def recursive_drop(grid, row_i, col_i, percentage_index): # too slow
+
+    while grid[row_i][col_i] != "^":
+        if row_i == len(grid)-1:
+            return 1
+
+        row_i += 1
+
+    if row_i == percentage_index:
+        print(".")
+
+    res = recursive_drop(grid, row_i, col_i+1, percentage_index)
+    if row_i == percentage_index:
+        print("Done")
+
+    res2 = recursive_drop(grid, row_i, col_i-1, percentage_index)
+    if row_i == percentage_index:
+        print("Done")
+
+    return res + res2
+
+def merge_beams(beams):
+
+    column_values = {}
+    for beam in beams:
+        if beam.column in column_values:
+            column_values[beam.column] += beam.value
+
+        else:
+            column_values[beam.column] = beam.value
+
+    new_beams = [Beam(column_value, column_values[column_value]) for column_value in column_values]
+
+    return new_beams
+
+
+
+def p2(grid):
+    beams = [
+        Beam(find_starting_column(grid[0]))
+    ]
+    row_index = 1
+
+    while row_index < len(grid):
+        row = grid[row_index]
+
+        new_beams = []
+        for beam_obj in beams:
+            if row[beam_obj.column] == "^":
+                new_beams.append(Beam(beam_obj.column-1, beam_obj.value))
+                new_beams.append(Beam(beam_obj.column+1, beam_obj.value))
+
+            else:
+                new_beams.append(Beam(beam_obj.column, beam_obj.value))
+
+        beams = merge_beams(new_beams)
+        row_index += 1
+
+    total_value = 0
+    for beam in beams:
+        total_value += beam.value
+
+    return total_value
+
+class Beam():
+    def __init__(self, column, value=1):
+        self.column = column
+        self.value = value
+
 grid = []
 
 with open("7/input.txt", "r") as file:
@@ -48,4 +117,12 @@ with open("7/input.txt", "r") as file:
 
 print(
     p1(grid)
+)
+
+#print(
+#    recursive_drop(grid, 0, find_starting_column(grid[0]), 20)
+#)
+
+print(
+    p2(grid)
 )
