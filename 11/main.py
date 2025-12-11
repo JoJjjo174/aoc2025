3c974d333b415a635a54e13c7294bb8afc12c9bc
Jonathan S <95241309+JoJjjo174@users.noreply.github.com>
2025-12-11 10:26:19 +0100

    d11p2

--- 11/main.py
+++ 11/main.py
@@ -1,17 +1,31 @@
+import functools
 
 class Node():
     def __init__(self, name, outwards_connections):
         self.name = name
         self.outwards_connections = outwards_connections
 
-    def discover(self):
-        if self.name == "out":
+    @functools.cache
+    def discover(self, visited_dac, visited_fft):#, already_visited):
+
+        if self.name == "out" and visited_dac and visited_fft:
             return 1
+        
+        #if self.name in already_visited:
+        #    return 0
+        
+        #already_visited.append(self.name)
+
+        if self.name == "dac":
+            visited_dac = True
+
+        elif self.name == "fft":
+            visited_fft = True
         
         total = 0
         for connection in self.outwards_connections:
             other_node = nodes[connection]
-            total += other_node.discover()
+            total += other_node.discover(visited_dac, visited_fft)#, already_visited.copy())
 
         return total
     
@@ -27,7 +41,20 @@ with open("11/input.txt", "r") as file:
 
 nodes["out"] = Node("out", [])
 
+
+
 print(
-    nodes["you"].discover()
+    nodes["you"].discover(True, True)#, [])
 )
-        
+
+#dac_connections = nodes["dac"].discover(True, True, [])
+#print(dac_connections)
+#dac_connections = 6651
+
+#fft_connections = nodes["fft"].discover(True, True)#, [])
+#print(fft_connections)
+
+print(
+    nodes["svr"].discover(False, False)#, [])
+)
+
